07f85c9ba05541c989e71fd8753190b70303b4f9
BooleanTron <booleantron@gmail.com>
2025-08-23 18:38:49 +0000

    Completed problem no 13👍

--- 13-chunk-array-into-groups/solution.js
+++ 13-chunk-array-into-groups/solution.js
@@ -2,16 +2,33 @@ function split(array, size) {
   if (array.length === 0) {
     return "invalid input";
   }
-  elseif(array.length <= size){
+  else if (array.length <= size) {
     return "no spliting possible";
   }
-
   let NSubArrays;
   if (array.length % size === 0) {
     NSubArrays = array.length / size;
   }
   else {
-    NSubArrays = Math.floor(array.length / size) + 1;
+    NSubArrays = Math.floor(array.length / size);
   }
 
+  let returnArray = [];
+  let temp = [];
+  for (element of array) {
+    temp.push(element);
+    if (temp.length === size) {
+      returnArray.push(temp);
+      temp = [];
+    }
+  }
+
+  if (temp.length > 0) {
+    returnArray.push(temp);
+  }
+
+  return returnArray;
 }
+
+const eg = [1, 2, 3, 4, 5];
+console.log(split(eg, 2));
